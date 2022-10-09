9525dac7634855a1a720fed1feb192e8b04a5541
ijcastrosena <111159778+ijcastrosena@users.noreply.github.com>
2022-10-08 20:31:46 -0500

    Listado contacto

--- G70/MiProyectoWEB/js/helpers/funciones.js
+++ G70/MiProyectoWEB/js/helpers/funciones.js
@@ -11,6 +11,15 @@ let datos = {}; // Objeto Literal
 
 // Bloque funciones
 
+const limpiarHTML = (listado) => {
+    const formulario = document.querySelector(`${listado}`);
+    if(formulario){
+        while(formulario.firstChild){
+            formulario.removeChild(formulario.firstChild);
+        };
+    };
+};
+
 const generarId = () =>{
     const randon = Math.random().toString(36).substr(2);
     const fecha = Date.now().toString(36);
@@ -118,11 +127,75 @@ export const validarFormulario = (e) =>{
 
 export const listadoContactos = () => {
 
-
     // Buscar el formulario y borrarlo
+    limpiarHTML("#form-contacto");
 
-    let seccion = document.querySelector('#form-contacto');
+    let tablaListado = document.querySelector('#form-contacto');
 
-    console.log(seccion);
+    tablaListado.innerHTML = `
+                                <main class="md:w-3/5  xl:w-4/5 px-5 py-10 bg-gray-200">
+                                    <div class="flex flex-col mt-100">
+                                        <div class="py-2 overflow-x-auto">
+                                            <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                                                <table class="min-w-full">
+                                                    <thead class="bg-gray-100 ">
+                                                        <tr>
+                                                            <th id="nombre" class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                                                                    Nombre Cliente
+                                                            </th>
+                                                            <th id="telefono" class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                                                                    Teléfono
+                                                            </th>
+                                                            <th id="correo" class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                                                                    Correo
+                                                            </th>
+                                                            <th id="mensaje" class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                                                                    Mensaje
+                                                            </th>
+                                                            <th id="acciones" class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                                                                Acciones
+                                                            </th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody id="listado-contactos" class="bg-white"
+                                                    </tbody>    
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </main>
+                            `;
+
+    // Listar Contactos en el HTML
+    contactos.forEach( (contacto ) => {
+        // Destructurar un objeto
+        const { nombre, telefono, correo, mensaje, id } = contacto;
+        const listadoContactos = document.querySelector('#listado-contactos');
+        if(listadoContactos){
+            listadoContactos.innerHTML += `
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                    <p class="text-gray-600 font-bold"> ${nombre} </p>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                                    <p class="text-gray-600">${telefono}</p>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-600">    
+                                                    <p class="text-gray-600">${correo}</p>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                                    <p class="text-gray-600">${mensaje}</p>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                                    <a id="editar" href="editar-contacto.html?id=${id}" data-contacto="${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                                                    <a id="eliminar" href="#" data-contacto="${id}" class="eliminar text-red-600 hover:text-red-900">Eliminar</a>
+                                                </td>
+                                            </tr>
+                                            `;
+        };
+    });
+
+    // Fin Listar Contactos
+
 
 };
