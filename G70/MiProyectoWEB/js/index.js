f69b9cd71504e1f7189beb84e40030c76721e93f
ijcastrosena <111159778+ijcastrosena@users.noreply.github.com>
2022-10-08 21:03:54 -0500

    Consultar API

--- G70/MiProyectoWEB/js/index.js
+++ G70/MiProyectoWEB/js/index.js
@@ -15,18 +15,27 @@ let enlaceListaContacto = document.querySelector("#lista-contacto");
 
 // Funciones
 
+const consultarApi = async () => {
+
+    try{
+        let respuesta = await fetch("https://min-api.cryptocompare.com/data/top/totalvolfull?limit=20&tsym=COP");
+        let resultados = await respuesta.json();
+        //console.log(resultados.Data[0].CoinInfo.FullName);
+        console.log(resultados.Data);
+
+    }catch(e){
+        console.error(e.message);
+    }
+
+}; 
 
 // Programa Principal
 // Escuchador de eventos
 
-
 document.addEventListener("DOMContentLoaded", () => {
-
-
     contactos = JSON.parse(localStorage.getItem("contactos")) || [];
     console.log(contactos);
     formulario.addEventListener("submit", validarFormulario );
-
     enlaceListaContacto.addEventListener("click", listadoContactos );
-
+    consultarApi();
 });
