2418d07977a09c6f4bc60b12db4445d8c3466b5f
ijcastrosena <111159778+ijcastrosena@users.noreply.github.com>
2022-10-27 21:51:27 -0500

    Componente detalle-producto

--- G70/adm-productos/src/pages/DetalleProducto.jsx
+++ G70/adm-productos/src/pages/DetalleProducto.jsx
@@ -1,19 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
 
 const DetalleProducto = () => {
 
+  const [ producto, setProducto ] = useState({});
+
+  const params = useParams();
+  const { id } = params;
+
+  useEffect( () => {
+    const consultarApi = async () => {
+        try {
+            const respuesta = await fetch(`https://whispering-wildwood-03076.herokuapp.com/guitarras/${id}`);
+            const resultados = await respuesta.json();
+            setProducto(resultados);
+        } catch (error) {
+            console.log("Error: " + error.message);
+        }
+    };
+    consultarApi();
+  },[]); 
+
+  const { _id, nombre, descripcion, precio, imagen } = producto;
+
   return (
-    <div>
-        DetalleProducto
-        <div>
-            <Link
-                to="/productos"
-            >
-                Regresa a Listado de Productos
-            </Link>
-        </div>
-    </div>
+    <>
+    {
+      producto._id 
+                ?
+                <div>
+                    
+                    <h3>
+                        <strong>{ nombre } </strong>
+                    </h3>
+                    <div>
+                      <img src={imagen.url} alt={nombre}/>
+                      <h4>Precio: $ { " " }  { precio } </h4>
+                      <p>
+                        { descripcion }
+                      </p>
+                    </div>                  
+
+                    <div>
+                        <Link
+                            to="/productos"
+                        >
+                            Regresa a Listado de Productos
+                        </Link>
+                    </div>
+                </div> 
+                :
+                <p>
+                  No hay informacion del producto, intentanuevamente.
+                </p>      
+    }
+    </>
   )
 }
 
